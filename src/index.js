449f816a56609a50b65db1ba732c61af8393825c
fainz777 <fainz777@gmail.com>
2019-09-25 09:59:09 +0300

    zeros/feature: solution [in progress]

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+	let zeros = 0;
+	const zeroEndedRegex = /0+$/;
+	const multipliers = expression.split('*');
+
+	multipliers.forEach(multiplier => {
+		const n = parseInt(multiplier);
+		let evens = 0;
+
+		for (let i = 1; i <= n; i++ ) {
+			const zeroEnded = i.toString().match(zeroEndedRegex);
+
+			if (zeroEnded) {
+				zeros += zeroEnded[0].length;
+			} else {
+				if (i % 2 === 0) {
+					evens++;
+				}
+
+				if (i % 5 === 0 && evens != 0) {
+					zeros++;
+					evens = 0;
+				}
+			}
+		}
+	});
+
+	return zeros;
 }
+
+//console.log(zeros('10!'))
