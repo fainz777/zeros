7fece7a80cee207c7139835001fa3ba647863211
fainz777 <fainz777@gmail.com>
2019-10-04 10:53:53 +0300

    zeros/draft: solution

--- src/index.js
+++ src/index.js
@@ -1,69 +1,101 @@
+function isDoubleFactorial(multiplier) {
+	return multiplier.indexOf('!!') !== -1;
+}
+
+function getDecimalLevel(n) {
+	return n.toString().length - 1;
+}
+
+
+
 module.exports = function zeros(expression) {
-	let zeros = 0;
-	//const zeroEndedRegex = /0+$/;
 	const multipliers = expression.split('*');
-	let fives = 0;
+	let zeros = 0;
+	let totalTwenties = 0;
+	let totalFives = 0;
 	let evens = 0;
-	//let tens = 0;
 
 	multipliers.forEach(multiplier => {
+		console.log('---------------------');
+		console.log('multiplier: ', multiplier)
 		let n = parseInt(multiplier);
-		debugger;
-		//let evens = 0;
+		const decimalLevel = getDecimalLevel(n); // Разряд числа
+		let hundrieds = 0;
 
-		if (n % 2 === 0)  {
-			evens++;
-		}
-
-		if (n % 5 === 0) {
-			fives++;
-		}
-
-		if (n % 10 === 0) {
-			zeros+= (+n.toString().length - 1);
+		if (decimalLevel > 1) {
+			hundrieds =  parseInt(n / Math.pow(10, decimalLevel));
 		}
 		
+		console.log('n: ', n);
+		console.log('Math.pow(10, decimalLevel): ', Math.pow(10, decimalLevel));
+		console.log('decimalLevel: ', decimalLevel);
+		console.log('hundrieds: ', hundrieds);
+		let tens = parseInt(n / 10);
 
-		if (multiplier.indexOf('!!') !== -1) {
-			if (n % 5 === 0) {
-				fives--;
-			}
+		console.log('n: ', n);
+		console.log('tens: ', tens);
 
-			if (n % 2 === 0) {
-				n-=2;
-				evens += parseInt(n / 2);
-				fives += (parseInt(n / 50) - 1);
-				zeros += parseInt(n / 10);
-				//tens += n % 10 === 0 ? 1 : 0;
-			} else {
-				//console.log(n)
-				fives += (parseInt(n / 5) - parseInt(n / 10));
-				fives += parseInt(n / 25);
-			}
-		} else {
-			if (n < 5) {
-				return;
-			}
-
-			if (n % 10 === 0) {
-				//zeros--;
-			}
-
-			let n25 = parseInt(n / 25)
-			zeros += n25;
-
-			n-=2;
-
-			evens += parseInt(n / 2);
-			fives += (parseInt(n / 5) - parseInt(n25 / 5));
+		if (n >= 100) {
+			tens += hundrieds;
 		}
-	});
 
+		console.log('tens: ', tens);
+		
+		if (isDoubleFactorial(multiplier)) {
+			if (n % 2 === 0) {
+				
+				let twenties = parseInt(n / 20) - hundrieds;
+				let fifties = 0;
+
+				if (n >=50) {
+					fifties = parseInt(n / 50) - hundrieds;
+				}
+				
+				evens += parseInt(n / 2);
+				totalTwenties += twenties;
+				zeros += tens;
+				totalFives += fifties;
+
+				console.log('totalFives 01: ', totalFives);
+			} else {
+				let fives = parseInt(n / 5) - tens;
+				console.log('fives: ', fives);
+				console.log('totalFives 1: ', totalFives);
+				totalFives += fives;
+				console.log('totalFives 2: ', totalFives);
+			}
+
+		} else {
+			let fives = parseInt(n / 5) - tens;
+			console.log('fives: ', fives);
+			let twenties = parseInt(n / 20) - hundrieds;
+			console.log('twenties: ', twenties);
+			let fifties = 0;
+
+			if (n >=50) {
+				fifties = parseInt(n / 50) - hundrieds;
+			}
+			evens += parseInt(n / 2);
+			totalTwenties += twenties;
+			totalFives += fives;
+			zeros += tens;
+			totalFives += fifties;
+		}
+
+		console.log('zeros loop end: ', zeros);
+	});
+console.log('**********************');
 	console.log('evens: ', evens);
-	console.log('fives: ', fives);
+	console.log('totalFives: ', totalFives);
+	console.log('totalTwenties: ', totalTwenties);
 	console.log('zeros: ', zeros);
 
-	zeros += evens > fives ? fives : evens;
+	zeros += evens > totalFives ? totalFives : evens;
+
+totalTwenties = totalTwenties ? totalFives : 0;
+//zeros -= totalTwenties;
+
+	zeros += totalTwenties;
 
 	//console.log('tens: ', tens);
 	console.log('zeros finale: ', zeros);
